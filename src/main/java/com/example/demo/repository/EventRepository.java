b8069d2d81553fe4c338eaabadb85d44bd022258
bala-amypo <bala@amypo.in>
2025-12-26 15:47:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/EventRepository.java
+++ src/main/java/com/example/demo/repository/EventRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.repository;
 
-import com.example.demo.config.entity.Event;
+import com.example.demo.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-
-    List<Event> findByActiveTrue();
+    List<Event> findByIsActiveTrue();
+    List<Event> findByIsActiveTrueAndCategory(String category);
+    List<Event> findByIsActiveTrueAndLocationContainingIgnoreCase(String location);
 }
