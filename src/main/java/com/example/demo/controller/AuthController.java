2113de028e1ed544e86de87305e53fbad0e8f144
bala-amypo <bala@amypo.in>
2025-12-26 16:02:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,23 +1,40 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.*;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.entity.User;
 import com.example.demo.security.JwtUtil;
+import com.example.demo.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final JwtUtil jwtUtil =
-            new JwtUtil("ThisIsAVerySecureSecretKeyForJwtDemo123456789", 3600000);
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    public AuthController(UserService userService, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
 
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest request) {
+
+        User user = userService.findByEmail(request.getEmail());
+
         AuthResponse response = new AuthResponse();
-        response.setEmail(request.getEmail());
-        response.setUserId(1L);
-        response.setRole("SUBSCRIBER");
-        response.setToken(jwtUtil.generateToken(1L, request.getEmail(), "SUBSCRIBER"));
+        response.setUserId(user.getId());
+        response.setEmail(user.getEmail());
+        response.setRole(user.getRole().name());
+        response.setToken(
+                jwtUtil.generateToken(
+                        user.getId(),
+                        user.getEmail(),
+                        user.getRole().name()
+                )
+        );
         return response;
     }
 }
