fd70d5bbe70dc91d7ba5a186cd83dda942786615
bala-amypo <bala@amypo.in>
2025-12-26 16:08:46 +0000

    User commit

--- src/main/java/com/example/demo/dto/EventUpdateRequest.java
+++ src/main/java/com/example/demo/dto/EventUpdateRequest.java
@@ -1,7 +1,32 @@
 package com.example.demo.dto;
 
 public class EventUpdateRequest {
-    public Long eventId;
-    public String updateContent;
-    public String updateType;
+
+    private Long eventId;
+    private String updateContent;
+    private String updateType; // INFO / WARNING / CRITICAL
+
+    public Long getEventId() {
+        return eventId;
+    }
+    
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+    
+    public String getUpdateContent() {
+        return updateContent;
+    }
+    
+    public void setUpdateContent(String updateContent) {
+        this.updateContent = updateContent;
+    }
+    
+    public String getUpdateType() {
+        return updateType;
+    }
+    
+    public void setUpdateType(String updateType) {
+        this.updateType = updateType;
+    }
 }
