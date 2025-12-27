b3acc9e27011978a5b7ad3fe85657435412e9cbe
bala-amypo <bala@amypo.in>
2025-12-27 13:55:39 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,19 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.context.event.EventListener;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(DemoApplication.class);
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-        Map<String, Object> props = new HashMap<>();
-        props.put("server.port", "9002"); // change port here
-        app.setDefaultProperties(props);
-
-        app.run(args);
+    @EventListener
+    public void onWebServerReady(WebServerInitializedEvent event) {
+        System.out.println("✅ Application running on port: " + event.getWebServer().getPort());
     }
 }
