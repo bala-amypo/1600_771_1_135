197103f146cdebdd846d7c9d203793728bd33819
bala-amypo <bala@amypo.in>
2025-12-27 13:52:39 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+
+        Map<String, Object> props = new HashMap<>();
+        props.put("server.port", "9002"); // change port here
+        app.setDefaultProperties(props);
+
+        app.run(args);
     }
 }
